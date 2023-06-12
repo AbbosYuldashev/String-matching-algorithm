a28889d8ca00c32ad5d97398182544e2a2a64db9
AbbosYuldashev <115147546+AbbosYuldashev@users.noreply.github.com>
2023-06-12 14:19:49 +0500

    Create Boyer_Moore_algorithm.cpp

---
+++ Boyer_Moore_algorithm.cpp
@@ -0,0 +1,61 @@
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <vector> 
+#include <unordered_map> 
+#include <chrono> 
+ 
+void computeBadCharacterTable(const std::string& pattern, std::unordered_map<char, int>& badCharTable) { 
+    int m = pattern.length(); 
+    for (int i = 0; i < m; i++) { 
+        badCharTable[pattern[i]] = i; 
+    } 
+} 
+ 
+void BoyerMooreSearch(const std::string& text, const std::string& pattern) { 
+    int n = text.length(); 
+    int m = pattern.length(); 
+ 
+    std::unordered_map<char, int> badCharTable; 
+    computeBadCharacterTable(pattern, badCharTable); 
+ 
+    int shift = 0; 
+    while (shift <= n - m) { 
+        int j = m - 1; 
+ 
+        while (j >= 0 && pattern[j] == text[shift + j]) { 
+            j--; 
+        } 
+ 
+        if (j < 0) { 
+            std::cout << "Pattern found at index " << shift << std::endl; 
+            shift += (shift + m < n) ? m - badCharTable[text[shift + m]] : 1; 
+        } else { 
+            int badCharShift = j - badCharTable[text[shift + j]]; 
+            shift += std::max(1, badCharShift); 
+        } 
+    } 
+} 
+ 
+int main() { 
+    std::ifstream file("strings.txt"); 
+    std::string text; 
+ 
+    if (!file.is_open()) { 
+        std::cerr << "Unable to open file" << std::endl; 
+        return 1; 
+    } 
+ 
+    text.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()); 
+ 
+    std::string pattern = "jF"; 
+ 
+    auto start = std::chrono::high_resolution_clock::now(); 
+    BoyerMooreSearch(text, pattern); 
+    auto end = std::chrono::high_resolution_clock::now(); 
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); 
+ 
+    std::cout << "Execution time: " << duration << " nanoseconds" << std::endl; 
+ 
+    return 0; 
+}

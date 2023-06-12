c08f02eda7cdef896d3f4d65ab352851c92ad5d3
AbbosYuldashev <115147546+AbbosYuldashev@users.noreply.github.com>
2023-06-12 14:12:35 +0500

    Update naive_algorithm.cpp

--- naive_algorithm.cpp
+++ naive_algorithm.cpp
@@ -1,23 +1,33 @@
-#include <iostream>#include <fstream>
-#include <string>#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
 void NaiveSearch(const std::string& text, const std::string& pattern) {
     int M = pattern.length();    int N = text.length();
     for (int i = 0; i <= N - M; i++) {
         int j;
-        for (j = 0; j < M; j++) {            if (text[i + j] != pattern[j])
+        for (j = 0; j < M; j++) {            
+            if (text[i + j] != pattern[j])
                 break;        }
         if (j == M) {
-            std::cout << "Pattern found at index " << i << std::endl;        }
-    }}
+            std::cout << "Pattern found at index " << i << std::endl;        
+        }
+    }
+}
 int main() {
-    std::ifstream file("strings.txt");    std::string text;
+    std::ifstream file("strings.txt");    
+    std::string text;
     if (!file.is_open()) {
-        std::cerr << "Unable to open file" << std::endl;        return 1;
+        std::cerr << "Unable to open file" << std::endl;        
+        return 1;
     }
     text.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
     std::string pattern = "DcoCtUkT4m";
-    auto start = std::chrono::high_resolution_clock::now();    NaiveSearch(text, pattern);
-    auto end = std::chrono::high_resolution_clock::now();    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    auto start = std::chrono::high_resolution_clock::now();    
+    
+    NaiveSearch(text, pattern);
+    auto end = std::chrono::high_resolution_clock::now();    
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     std::cout << "Execution time: " << duration << " nanoseconds" << std::endl;
     return 0;
 }

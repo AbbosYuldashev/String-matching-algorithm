b86beca838233eb0d3d6159c119ffffe6f15f609
AbbosYuldashev <115147546+AbbosYuldashev@users.noreply.github.com>
2023-06-12 22:09:10 +0500

    Update naive_algorithm.cpp

--- naive_algorithm.cpp
+++ naive_algorithm.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <chrono>
+using namespace std;
+using namespace std::chrono;
 void NaiveSearch(const std::string& text, const std::string& pattern) {
+    
     int M = pattern.length();    int N = text.length();
     for (int i = 0; i <= N - M; i++) {
         int j;
-        for (j = 0; j < M; j++) {            
+        for (j = 0; j < M; j++) {
             if (text[i + j] != pattern[j])
                 break;        }
         if (j == M) {
-            std::cout << "Pattern found at index " << i << std::endl;        
+            std::cout << "Pattern found at index " << i << std::endl;
         }
     }
+    
 }
-int main() {
-    std::ifstream file("strings.txt");    
+int mains() {
+    std::ifstream file("strings.txt");
     std::string text;
     if (!file.is_open()) {
-        std::cerr << "Unable to open file" << std::endl;        
+        std::cerr << "Unable to open file" << std::endl;
         return 1;
     }
     text.assign(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-    std::string pattern = "DcoCtUkT4m";
-    auto start = std::chrono::high_resolution_clock::now();    
-    
+    std::string pattern = "jF";
+
+    auto start = high_resolution_clock::now();
     NaiveSearch(text, pattern);
-    auto end = std::chrono::high_resolution_clock::now();    
-    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-    std::cout << "Execution time: " << duration << " nanoseconds" << std::endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;
+
     return 0;
 }
